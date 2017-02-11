89970aa3c00100ef2c5ffdb2ec66e51200940414
Shadizzle <mail@shayden.me>
2017-02-11 18:10:19 -0300

    Limpié el core.

--- src/demo_recruitment_app/core.cljs
+++ src/demo_recruitment_app/core.cljs
@@ -1,16 +1,8 @@
 (ns demo-recruitment-app.core
-  (:require ))
+  (:require))
 
 (enable-console-print!)
 
-(println "This text is printed from src/demo-recruitment-app/core.cljs. Go ahead and edit it and see reloading in action.")
-
-;; define your app data so that it doesn't get over-written on reload
+(defn on-js-reload [])
 
 (defonce app-state (atom {:text "Hello world!"}))
-
-(defn on-js-reload []
-  ;; optionally touch your app-state to force rerendering depending on
-  ;; your application
-  ;; (swap! app-state update-in [:__figwheel_counter] inc)
-)
