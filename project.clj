c1304966f8d6a95287e144114fbe6cbf21660712
Shadizzle <mail@shayden.me>
2017-02-11 20:18:47 -0300

    Primer paso, un listado simple de usuarios

--- project.clj
+++ project.clj
@@ -5,7 +5,9 @@
   :dependencies [[org.clojure/clojure "1.8.0"]
                  [org.clojure/clojurescript "1.9.229"]
                  [org.clojure/core.async "0.2.395"
-                  :exclusions [org.clojure/tools.reader]]]
+                  :exclusions [org.clojure/tools.reader]]
+                 [cljs-http "0.1.42"]
+                 [reagent "0.6.0"]]
 
   :plugins [[lein-figwheel "0.5.9"]
             [lein-cljsbuild "1.1.5"
--- src/demo_recruitment_app/core.cljs
+++ src/demo_recruitment_app/core.cljs
@@ -1,8 +1,29 @@
 (ns demo-recruitment-app.core
-  (:require))
+  (:require-macros [cljs.core.async.macros :refer [go]])
+  (:require [cljs-http.client :as http]
+            [reagent.core :as r]
+            [cljs.core.async :refer [<!]]))
 
 (enable-console-print!)
 
 (defn on-js-reload [])
 
-(defonce app-state (atom {:text "Hello world!"}))
+(defonce gh-users (r/atom []))
+
+(go
+  (let [response (<! (http/get "https://api.github.com/search/users?q=type:user&per_page=100"
+                               {:with-credentials? false}))
+        users (:items (:body response))]
+    (swap! gh-users #(into % users))))
+
+(defn user-list [users]
+  (js/console.log (str users))
+  (if (seq users)
+    (into [:ul] (map #(vector :li (:login %)) users))
+    [:span "waiting"]))
+
+(defn root []
+  [user-list @gh-users])
+
+(r/render-component [root]
+  (.getElementById js/document "app"))
