3e0a4c6c472288507d4d0890452c70166523fe1a
Shadizzle <mail@shayden.me>
2017-02-11 17:39:50 -0300

    Inicié el proyecto con 'lein new figwheel' y limpié la configuración que fue generada por esa plantilla.

---
+++ README.md
@@ -0,0 +1,20 @@
+# demo-recruitment-app
+
+Reclutamiento del futuro. Estupendo, realmente estupendo.
+
+## Dependencies
+
+- [Java JDK 6 o mas actual](http://www.oracle.com/technetwork/java/javase/downloads/index.html)
+- [Leiningen](https://leiningen.org/#install)
+
+## Setup
+
+Desde la carpeta base del proyecto, corre este comando para iniciar un servidor de desarrollo:
+
+    `lein figwheel`
+
+Para producir una versión que se puede utilizar en producción:
+
+    `lein do clean, cljsbuild once min`
+
+Mas información en [el repositorio de Figwheel](https://github.com/bhauman/lein-figwheel).
---
+++ project.clj
@@ -0,0 +1,41 @@
+(defproject demo-recruitment-app "0.1.0"
+  :url ""
+  :min-lein-version "2.7.0"
+
+  :dependencies [[org.clojure/clojure "1.8.0"]
+                 [org.clojure/clojurescript "1.9.229"]
+                 [org.clojure/core.async "0.2.395"
+                  :exclusions [org.clojure/tools.reader]]]
+
+  :plugins [[lein-figwheel "0.5.9"]
+            [lein-cljsbuild "1.1.5"
+             :exclusions [[org.clojure/clojure]]]]
+
+  :source-paths ["src"]
+
+  :clean-targets ^{:protect false} ["resources/public/js/compiled" "target"]
+
+  :cljsbuild {:builds
+              [{:id "dev"
+                :source-paths ["src"]
+                :figwheel {:on-jsload "demo-recruitment-app.core/on-js-reload"}
+                :compiler {:main demo-recruitment-app.core
+                           :asset-path "js/compiled/out"
+                           :output-to "resources/public/js/compiled/demo_recruitment_app.js"
+                           :output-dir "resources/public/js/compiled/out"
+                           :source-map-timestamp true
+                           :preloads [devtools.preload]}}
+               {:id "min"
+                :source-paths ["src"]
+                :compiler {:output-to "resources/public/js/compiled/demo_recruitment_app.js"
+                           :main demo-recruitment-app.core
+                           :optimizations :advanced
+                           :pretty-print false}}]}
+
+  :figwheel {:css-dirs ["resources/public/css"]}
+
+  :profiles {:dev {:dependencies [[binaryage/devtools "0.9.0"]
+                                  [figwheel-sidecar "0.5.9"]
+                                  [com.cemerick/piggieback "0.2.1"]]
+                   :source-paths ["src" "dev"]
+                   :repl-options {:nrepl-middleware [cemerick.piggieback/wrap-cljs-repl]}}})
