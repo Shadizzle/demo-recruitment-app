be462d34f25328fd4f955a2d18ad1117e77a0cf9
Shadizzle <mail@shayden.me>
2017-02-14 00:50:28 -0300

    Filtros casi terminados.

--- src/demo_recruitment_app/core.cljs
+++ src/demo_recruitment_app/core.cljs
@@ -3,20 +3,23 @@
   (:require [cljs-http.client :as http]
             [reagent.core :as r]
             [demo-recruitment-app.storage :as s]
+            [clojure.string :refer [blank?]]
             [cljs.core.async :refer [chan <!]]))
 
 (enable-console-print!)
 
 (defn on-js-reload [])
 
-(defonce gh-users (r/atom []))
-(defonce gh-query (r/atom {}))
-(defonce favourites (r/atom false))
+;; State
 
-(defn get-users [q]
-  (let [response-chan (chan 1 (map #(get-in % [:body :items])))
-        url "https://api.github.com/search/users?per_page=100&q=type:user"]
-    (http/get url {:with-credentials? false :channel response-chan})))
+(defonce gh-users (r/atom []))
+(defonce gh-query (r/atom {:followers ""
+                           :location ""
+                           :language ""}))
+(defonce gh-query-page (r/atom 1))
+(defonce only-favourites (r/atom false))
+
+;; Helpers
 
 (defn tag-favourite [user]
   (let [fav-set (s/get-favourites)
@@ -25,30 +28,59 @@
                    [:favourite true]
                    [:favourite false]))))
 
-(defn reset-users! [users query]
+(defn make-url [query page]
+  (str "https://api.github.com/search/users?per_page=100&page=" page
+       "&q=type:user"
+       (if-not (-> query :followers blank?)
+         (str " followers:" (:followers query)))
+       (if-not (-> query :location blank?)
+         (str " location:" (:location query)))
+       (if-not (-> query :language blank?)
+         (str " language:" (:language query)))))
+
+;; Actions
+
+(defn get-users [query page]
+  (let [response-chan (chan 1 (map #(get-in % [:body :items])))
+        url (make-url query page)]
+    (http/get url {:with-credentials? false :channel response-chan})))
+
+(defn reset-users! []
   (go
-    (let [new-users (<! (get-users @query))]
-      (reset! users (map tag-favourite new-users)))))
+    (let [new-users (<! (get-users @gh-query @gh-query-page))]
+      (reset! gh-users (map tag-favourite new-users)))))
 
-(defn into-users! [users query]
+(defn into-users! []
   (go
-    (let [new-users (<! (get-users @query))]
-      (swap! users #(into % (map tag-favourite) new-users)))))
+    (let [new-users (<! (get-users @gh-query @gh-query-page))]
+      (swap! gh-users #(into % (map tag-favourite) new-users)))))
 
-(defonce _ (reset-users! gh-users gh-query))
-
-(defn favourite-user [user]
+(defn favourite-user! [user]
   (s/update-favourites! conj (:id user))
   (swap! gh-users #(map tag-favourite %)))
 
-(defn unfavourite-user [user]
+(defn unfavourite-user! [user]
   (s/update-favourites! disj (:id user))
   (swap! gh-users #(map tag-favourite %)))
 
+(defn update-query! []
+  (let [followers-val (.-value (.getElementById js/document "followers-filter"))
+        location-val (.-value (.getElementById js/document "location-filter"))
+        language-val (.-value (.getElementById js/document "language-filter"))
+        new-query {:followers followers-val
+                   :location location-val
+                   :language language-val}]
+    (when (not= @gh-query new-query)
+      (reset! gh-query new-query)
+      (reset! gh-query-page 1)
+      (reset-users!))))
+
+;; Components
+
 (defn favourite-star [user]
   (if (:favourite user)
-    [:span.fa.fa-star {:on-click #(unfavourite-user user)}]
-    [:span.fa.fa-star-o {:on-click #(favourite-user user)}]))
+    [:span.fa.fa-star {:on-click #(unfavourite-user! user)}]
+    [:span.fa.fa-star-o {:on-click #(favourite-user! user)}]))
 
 (defn user-row [user]
   [:tr
@@ -59,7 +91,7 @@
   (if (seq @gh-users)
     [:table.table.table-striped.user-list
       [:tbody
-        (let [show-all (not @favourites)]
+        (let [show-all (not @only-favourites)]
           (for [user @gh-users
                 :when (or show-all
                           (:favourite user))]
@@ -67,17 +99,38 @@
     [:div.loading-spinner [:span.fa.fa-spinner.fa-spin]]))
 
 (defn favourites-filter []
-  [:div.checkbox
+  [:div.checkbox.favourites-filter
     [:label {:for "favourites-filter"}
       [:input#favourites-filter
         {:type "checkbox"
-         :value @favourites
-         :on-click #(swap! favourites not)}]
+         :value @only-favourites
+         :on-click #(swap! only-favourites not)}]
       "Mostrar solo favoritos"]])
+
+(defn query-filters []
+  [:div.form-horizontal.query-filters
+    [:div.form-group.followers-filter
+      [:label.control-label.col-sm-4 {:for "followers-filter"}
+        "Seguidores"]
+      [:div.col-sm-8
+        [:input#followers-filter.form-control]]]
+    [:div.form-group.location-filter
+      [:label.control-label.col-sm-4 {:for "location-filter"}
+        "Ubicación"]
+      [:div.col-sm-8
+        [:input#location-filter.form-control]]]
+    [:div.form-group.language-filter
+      [:label.control-label.col-sm-4 {:for "language-filter"}
+        "Lenguaje"]
+      [:div.col-sm-8
+        [:input#language-filter.form-control]]]
+    [:input.btn.btn-default.col-sm-12 {:type "button" :value "Filtrar"
+                                         :on-click #(update-query!)}]])
 
 (defn filter-list []
   [:div.filter-list
-    [favourites-filter]])
+    [favourites-filter]
+    [query-filters]])
 
 (defn root []
   [:div.container
@@ -85,6 +138,10 @@
     [:div.row
       [:div.col-md-8 [user-list]]
       [:div.col-md-3 [filter-list]]]])
+
+;; Run
+
+(defonce users-initialised (reset-users!))
 
 (r/render-component [root]
   (.getElementById js/document "app"))
